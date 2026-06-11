b527ba7b2415f90510caa4ed7c8b46a66cf77251
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-06-11 14:04:56 +0530

    Time: 2 ms (71.66%), Space: 44.8 MB (33.66%)

---
+++ Week_2/Day_4/3. Generate Parentheses/Code.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        helper(result, "", n, n);
+        
+        return result;
+    }
+
+    private void helper(List<String> result, String current, int open, int close) {
+        if (open == 0 && close == 0) {
+            result.add(current);
+            return;
+        }
+
+        if (open > 0) {
+            helper(result, current + "(", open - 1, close);
+        }
+
+        if (close > open) {
+            helper(result, current + ")", open, close - 1);
+        }
+    }
+}
