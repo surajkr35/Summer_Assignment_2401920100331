bf7a3ec5c26cae7d56774189236cd9e044bad7f6
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-06-20 17:07:47 +0530

    Time: 69 ms (54.16%), Space: 78.1 MB (59.72%)

---
+++ Week_3/Day_4/3. Largest Rectangle in Histogram/Code.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+
+        for(int i = 0; i <= n; i++){
+            int curr = (i == n) ? 0 : heights[i];
+
+            while(!stack.isEmpty() && curr < heights[stack.peek()]){
+                int height = heights[stack.pop()];
+
+                int width;
+                if(stack.isEmpty()){
+                    width = i;
+                }
+                
+                else{
+                    width = i - stack.peek() - 1;
+                }
+
+                maxArea = Math.max(maxArea, height * width);
+            }
+
+            stack.push(i);
+        }
+
+        return maxArea;
+    }
+}
