cab0d075cae2c80cbe584085f082b7c968a2831d
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-06-20 17:00:40 +0530

    Time: 6 ms (88.52%), Space: 45.4 MB (45.38%)

---
+++ Week_3/Day_4/2. Evaluate Reverse Polish Notation/Code.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+
+            if (!token.equals("+") && !token.equals("-") &&
+                !token.equals("*") && !token.equals("/")) {
+                stack.push(Integer.parseInt(token));
+            }
+
+            else {
+                int b = stack.pop();
+                int a = stack.pop();
+
+                if (token.equals("+")) {
+                    stack.push(a + b);
+                } 
+                else if (token.equals("-")) {
+                    stack.push(a - b);
+                } 
+                else if (token.equals("*")) {
+                    stack.push(a * b);
+                } 
+                else {
+                    stack.push(a / b);
+                }
+            }
+        }
+
+        return stack.pop();
+    }
+}
