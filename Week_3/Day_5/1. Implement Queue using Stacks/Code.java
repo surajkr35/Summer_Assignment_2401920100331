6aac741ede622d11ccaa920e91c75934153df1b8
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-06-20 17:36:14 +0530

    Time: 0 ms (100%), Space: 42.8 MB (43.6%)

---
+++ Week_3/Day_5/1. Implement Queue using Stacks/Code.java
@@ -0,0 +1,32 @@
+class MyQueue {
+
+    Stack<Integer> inStack = new Stack<>();
+    Stack<Integer> outStack = new Stack<>();
+
+    public MyQueue(){
+        
+    }
+    
+    public void push(int x){
+        inStack.push(x);
+    }
+    
+    public int pop(){
+        peek();
+        return outStack.pop();
+    }
+    
+    public int peek() {
+        if(outStack.isEmpty()){
+
+            while(!inStack.isEmpty()){
+                outStack.push(inStack.pop());
+            }
+        }
+        return outStack.peek();
+    }
+    
+    public boolean empty() {
+        return inStack.isEmpty() && outStack.isEmpty();
+    }
+}
