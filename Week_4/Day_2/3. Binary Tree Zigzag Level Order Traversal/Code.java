c7086695cf3bb1874da5130933c926b90ccbe1f9
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-06-29 10:32:13 +0530

    Time: 1 ms (79.32%), Space: 43.5 MB (90.5%)

---
+++ Week_4/Day_2/3. Binary Tree Zigzag Level Order Traversal/Code.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        if(root == null){
+            return ans;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        boolean leftToRight = true;
+
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+
+            for(int i = 0; i < size; i++){
+                TreeNode curr = queue.poll();
+
+                if(leftToRight){
+                    level.add(curr.val);
+                }
+                else{
+                    level.add(0, curr.val);
+                }
+
+                if(curr.left != null){
+                    queue.offer(curr.left);
+                }
+
+                if(curr.right != null){
+                    queue.offer(curr.right);
+                }
+            }
+
+            ans.add(level);
+            leftToRight = !leftToRight;
+        }
+
+        return ans;
+    }
+}
